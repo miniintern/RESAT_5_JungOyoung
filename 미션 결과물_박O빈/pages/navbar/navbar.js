62043a4ec217873fca2cec0b64036146a067c835
miniintern <144213890+miniintern@users.noreply.github.com>
2023-12-13 13:12:39 +0900

    Delete 미션 결과물_박O빈 directory

--- 미션 결과물_박O빈/pages/navbar/navbar.js
+++
@@ -1,6 +0,0 @@
-const navMenu = document.querySelector(".navbar-menu");
-const navButton = document.querySelector(".navbar-button");
-
-navButton.addEventListener("click", () => {
-  navMenu.classList.toggle("active");
-});
